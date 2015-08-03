a8a6d397d709ce09d92bf9c2e2b792db950c10c2
Ilja P <iljapanic@gmail.com>
2015-08-03 05:00:38 +0200

    splash colophon with hover effect

--- js/main.js
+++ js/main.js
@@ -13,8 +13,10 @@ window.onresize = function() {
 
 window.onload = function() {
     
-    var activator = document.querySelectorAll('.social-media li');
+    var activator = document.querySelectorAll('.splash__social li');
+    var activator2 = document.querySelectorAll('.splash__colophon a');
     var splash =  document.getElementById("splash");
+
 
 
     for (var i in activator) {
@@ -27,6 +29,19 @@ window.onload = function() {
         activator[i].addEventListener( 'mouseout', function() {
             classie.add( splash, 'splash-1' );
             classie.remove( splash, 'splash-2' );
+        })
+    }
+
+    for (var i in activator2) {
+        if (!activator2.hasOwnProperty(i)) continue;
+        activator2[i].addEventListener( 'mouseover', function() {
+            // document.body.className = 'splash-1';
+            classie.add( splash, 'splash-3' );
+            classie.remove( splash, 'splash-1' );
+        })
+        activator2[i].addEventListener( 'mouseout', function() {
+            classie.add( splash, 'splash-1' );
+            classie.remove( splash, 'splash-3' );
         })
     }
 }
