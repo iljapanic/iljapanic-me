e760d699bb4e65b0bae3a69494879429e8517278
Ilja <ilja@iljapanic.me>
2018-03-14 16:42:54 +0000

    refactor scripts, add lazy load to all images

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,87 +1,12 @@
-// Show an element
-var show = function (elem) {
+/* https://github.com/toddmotto/echo */
+!function(t,e){"function"==typeof define&&define.amd?define(function(){return e(t)}):"object"==typeof exports?module.exports=e:t.echo=e(t)}(this,function(t){"use strict";var e,n,o,r,c,a={},u=function(){},d=function(t){return null===t.offsetParent},l=function(t,e){if(d(t))return!1;var n=t.getBoundingClientRect();return n.right>=e.l&&n.bottom>=e.t&&n.left<=e.r&&n.top<=e.b},i=function(){(r||!n)&&(clearTimeout(n),n=setTimeout(function(){a.render(),n=null},o))};return a.init=function(n){n=n||{};var d=n.offset||0,l=n.offsetVertical||d,f=n.offsetHorizontal||d,s=function(t,e){return parseInt(t||e,10)};e={t:s(n.offsetTop,l),b:s(n.offsetBottom,l),l:s(n.offsetLeft,f),r:s(n.offsetRight,f)},o=s(n.throttle,250),r=n.debounce!==!1,c=!!n.unload,u=n.callback||u,a.render(),document.addEventListener?(t.addEventListener("scroll",i,!1),t.addEventListener("load",i,!1)):(t.attachEvent("onscroll",i),t.attachEvent("onload",i))},a.render=function(n){for(var o,r,d=(n||document).querySelectorAll("[data-echo], [data-echo-background]"),i=d.length,f={l:0-e.l,t:0-e.t,b:(t.innerHeight||document.documentElement.clientHeight)+e.b,r:(t.innerWidth||document.documentElement.clientWidth)+e.r},s=0;i>s;s++)r=d[s],l(r,f)?(c&&r.setAttribute("data-echo-placeholder",r.src),null!==r.getAttribute("data-echo-background")?r.style.backgroundImage="url("+r.getAttribute("data-echo-background")+")":r.src!==(o=r.getAttribute("data-echo"))&&(r.src=o),c||(r.removeAttribute("data-echo"),r.removeAttribute("data-echo-background")),u(r,"load")):c&&(o=r.getAttribute("data-echo-placeholder"))&&(null!==r.getAttribute("data-echo-background")?r.style.backgroundImage="url("+o+")":r.src=o,r.removeAttribute("data-echo-placeholder"),u(r,"unload"));i||a.detach()},a.detach=function(){document.removeEventListener?t.removeEventListener("scroll",i):t.detachEvent("onscroll",i),clearTimeout(n)},a});
 
-  // Get the natural height of the element
-  var getHeight = function () {
-    elem.style.display = 'block'; // Make it visible
-    var height = elem.scrollHeight + 'px'; // Get it's height
-    elem.style.display = ''; //  Hide it again
-    return height;
-  };
-
-  var height = getHeight(); // Get the natural height
-  elem.classList.add('is-visible'); // Make the element visible
-  elem.style.height = height; // Update the max-height
-
-  // Once the transition is complete, remove the inline max-height so the content can scale responsively
-  window.setTimeout(function () {
-    elem.style.height = '';
-  }, 350);
-
+function domReady() {
+  echo.init({
+    offset: 0,
+    throttle: 15,
+    unload: false
+  });
 };
 
-// Hide an element
-var hide = function (elem) {
-
-  // Give the element a height to change from
-  elem.style.height = elem.scrollHeight + 'px';
-
-  // Set the height back to 0
-  window.setTimeout(function () {
-    elem.style.height = '0';
-  }, 1);
-
-  // When the transition is complete, hide it
-  window.setTimeout(function () {
-    elem.classList.remove('is-visible');
-  }, 350);
-
-};
-
-// Toggle element visibility
-var toggle = function (elem, timing) {
-
-  // If the element is visible, hide it
-  if (elem.classList.contains('is-visible')) {
-    hide(elem);
-    return;
-  }
-
-  // Otherwise, show it
-  show(elem);
-
-};
-
-var toggleButton = function(elem, timing) {
-  // If the element is visible, hide it
-  if (elem.classList.contains('show-more')) {
-    elem.classList.add('show-less');
-    elem.classList.remove('show-more');
-    return;
-  }
-
-  elem.classList.add('show-more');
-  elem.classList.remove('show-less');
-}
-
-function ready() {
-  document.addEventListener('click', function (event) {
-
-    // Make sure clicked element is our toggle
-    if (!event.target.classList.contains('toggle')) return;
-
-    // Prevent default link behavior
-    event.preventDefault();
-
-    // Get the content
-    var content = document.querySelector(event.target.hash);
-    if (!content) return;
-
-    // Toggle the content
-    toggle(content);
-    toggleButton(event.target);
-
-  }, false);
-}
-
-document.addEventListener('DOMContentLoaded', ready);
+document.addEventListener('DOMContentLoaded', domReady);
