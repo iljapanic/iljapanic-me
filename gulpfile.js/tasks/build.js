42d795f5779a5c111467fa73c84975acf9c30127
Ilja <ilja@iljapanic.me>
2018-03-15 15:35:31 +0000

    add clean task

--- gulpfile.js/tasks/build.js
+++ gulpfile.js/tasks/build.js
@@ -2,5 +2,5 @@ const gulp = require('gulp');
 const runSequence = require('run-sequence');
 
 gulp.task('build', function(cb) {
-  runSequence('assets', 'jekyll-build-once', cb);
+  runSequence('clean', 'assets', 'jekyll-build-once', cb);
 });
--- notes.md
+++
@@ -1,8 +0,0 @@
----
-layout: notes
-title: Notes
-permalink: /notes/
-group: 'navigation'
----
-
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna.
