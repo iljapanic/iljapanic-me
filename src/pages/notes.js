84e327c450eefa56edf3483aa98bbb8b90135b6e
iljapanic <ilja@iljapanic.com>
2020-10-11 02:30:15 +0200

    change component cases

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Note from '../components/Note'
+import Note from '../components/note'
 import Headline from '../components/headline'
 
 const NotesPage = ({ data, location }) => {
