626821c982e4a2cbc762abe5db4b210f9ae37657
iljapanic <ilja@iljapanic.com>
2021-01-06 17:31:05 +0100

    generate title for soundcloud iframe embeds, update music page title

--- src/components/molecules/soundcloud.js
+++ src/components/molecules/soundcloud.js
@@ -19,12 +19,16 @@ const Soundcloud = (props) => {
     ? `https://w.soundcloud.com/player/?url=${url}&amp;auto_play=${autoPlay}&amp;hide_related=${hideRelated}&amp;show_comments=${showComments}&amp;show_user=${showUser}&amp;show_reposts=${showReposts}&amp;visual=${visual}`
     : `https://w.soundcloud.com/player/?url=${url}&amp;color=${color}&amp;auto_play=${autoPlay}&amp;hide_related=${hideRelated}&amp;show_comments=${showComments}&amp;show_user=${showUser}&amp;show_reposts=${showReposts}&amp;show_teaser=${showTeaser}`
 
+  var urlParts = url.split('/')
+  var urlTitle = urlParts[urlParts.length - 1]
+
   return (
     <iframe
+      title={`soundcloud-embed-${urlTitle}`}
       width={width}
       height={height}
       scrolling="no"
-      frameborder="no"
+      frameBorder="no"
       src={src}
       className="mt"
     />
