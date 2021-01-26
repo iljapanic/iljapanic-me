5e1fc494347e7d24ee6beb818815b6b19ce5bb3a
iljapanic <ilja@iljapanic.com>
2021-01-26 13:30:28 +0100

    update packages, add projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,28 +2,25 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Headline from '../components/headline'
 
-const IndexPage = ({ data, location }) => {
+const ProjectsPage = ({ data, location }) => {
   return (
     <Layout location={location}>
-      <SEO title="Feed" />
+      <SEO title="Articles" />
       <section className="container">
-        <h1 className="text-center">Projects</h1>
+        <div className="content-column">
+          <Headline title="Projects" headline="Current and past works" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            delectus blanditiis laborum alias, corporis maiores adipisci
+            perspiciatis. Odio laboriosam voluptatibus officiis quo dicta
+            excepturi. Quos voluptatibus sequi ipsum cum consequatur!
+          </p>
+        </div>
       </section>
     </Layout>
   )
 }
 
-export const query = graphql`
-  query {
-    illustration: file(relativePath: { eq: "hero.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 800) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`
-
-export default IndexPage
+export default ProjectsPage
