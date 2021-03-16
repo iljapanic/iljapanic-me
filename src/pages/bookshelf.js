ab0225be6e06f9ea6c614d6d6889e26b051723c6
iljapanic <ilja@iljapanic.com>
2021-03-16 20:45:39 +0100

    finally fixed airtable fetching, it was a damn typo...

--- src/pages/bookshelf.js
+++ src/pages/bookshelf.js
@@ -6,41 +6,41 @@ import BookPreview from '../components/previews/bookPreview.js'
 import Headline from '../components/molecules/headline.js'
 
 const Bookshelf = ({ data, location }) => {
-  // const nonFiction = data.nonFiction.edges.map((item) => {
-  //   const id = item.node.id
-  //   const book = item.node.data
-  //   return (
-  //     <BookPreview
-  //       key={id}
-  //       title={book.title}
-  //       year={book.year}
-  //       url={book.goodreads}
-  //       collection={book.collection}
-  //       featured={book.featured}
-  //       authors={book.authors}
-  //       tags={book.tags}
-  //       cover={book.cover.localFiles[0].childImageSharp.fluid}
-  //     />
-  //   )
-  // })
+  const nonFiction = data.nonFiction.edges.map((item) => {
+    const id = item.node.id
+    const book = item.node.data
+    return (
+      <BookPreview
+        key={id}
+        title={book.title}
+        year={book.year}
+        url={book.goodreads}
+        collection={book.collection}
+        featured={book.featured}
+        authors={book.people}
+        tags={book.tags}
+        cover={book.cover.localFiles[0].childImageSharp.fluid}
+      />
+    )
+  })
 
-  // const fiction = data.fiction.edges.map((item) => {
-  //   const id = item.node.id
-  //   const book = item.node.data
-  //   return (
-  //     <BookPreview
-  //       key={id}
-  //       title={book.title}
-  //       year={book.year}
-  //       url={book.goodreads}
-  //       collection={book.collection}
-  //       featured={book.featured}
-  //       authors={book.authors}
-  //       tags={book.tags}
-  //       cover={book.cover.localFiles[0].childImageSharp.fluid}
-  //     />
-  //   )
-  // })
+  const fiction = data.fiction.edges.map((item) => {
+    const id = item.node.id
+    const book = item.node.data
+    return (
+      <BookPreview
+        key={id}
+        title={book.title}
+        year={book.year}
+        url={book.goodreads}
+        collection={book.collection}
+        featured={book.featured}
+        authors={book.people}
+        tags={book.tags}
+        cover={book.cover.localFiles[0].childImageSharp.fluid}
+      />
+    )
+  })
 
   return (
     <Layout location={location}>
@@ -56,7 +56,7 @@ const Bookshelf = ({ data, location }) => {
           {/* non-fiction */}
           <section>
             <h2>Non-fiction</h2>
-            {/* <div className="books">{nonFiction}</div> */}
+            <div className="books">{nonFiction}</div>
           </section>
 
           {/* separator */}
@@ -65,7 +65,7 @@ const Bookshelf = ({ data, location }) => {
           {/* fiction */}
           <section>
             <h2>Fiction</h2>
-            {/* <div className="books">{fiction}</div> */}
+            <div className="books">{fiction}</div>
           </section>
         </div>
       </div>
@@ -73,86 +73,86 @@ const Bookshelf = ({ data, location }) => {
   )
 }
 
-// export const query = graphql`
-//   query {
-//     nonFiction: allAirtable(
-//       filter: {
-//         table: { eq: "books" }
-//         data: { category: { eq: "non-fiction" } }
-//       }
-//       sort: { fields: data___authors___data___last_name, order: ASC }
-//     ) {
-//       edges {
-//         node {
-//           id
-//           data {
-//             collection
-//             goodreads
-//             year
-//             title
-//             featured
-//             authors {
-//               data {
-//                 first_name
-//                 last_name
-//               }
-//             }
-//             tags {
-//               data {
-//                 tag
-//               }
-//             }
-//             cover {
-//               localFiles {
-//                 childImageSharp {
-//                   fluid(maxWidth: 400) {
-//                     ...GatsbyImageSharpFluid
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//     fiction: allAirtable(
-//       filter: { table: { eq: "books" }, data: { category: { eq: "fiction" } } }
-//       sort: { fields: data___authors___data___last_name, order: ASC }
-//     ) {
-//       edges {
-//         node {
-//           id
-//           data {
-//             collection
-//             goodreads
-//             year
-//             title
-//             featured
-//             authors {
-//               data {
-//                 first_name
-//                 last_name
-//               }
-//             }
-//             tags {
-//               data {
-//                 tag
-//               }
-//             }
-//             cover {
-//               localFiles {
-//                 childImageSharp {
-//                   fluid(maxWidth: 400) {
-//                     ...GatsbyImageSharpFluid
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query {
+    nonFiction: allAirtable(
+      filter: {
+        table: { eq: "books" }
+        data: { category: { eq: "non-fiction" } }
+      }
+      sort: { fields: data___people___data___last_name, order: ASC }
+    ) {
+      edges {
+        node {
+          id
+          data {
+            collection
+            goodreads
+            year
+            title
+            featured
+            people {
+              data {
+                first_name
+                last_name
+              }
+            }
+            tags {
+              data {
+                tag
+              }
+            }
+            cover {
+              localFiles {
+                childImageSharp {
+                  fluid(maxWidth: 400) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    fiction: allAirtable(
+      filter: { table: { eq: "books" }, data: { category: { eq: "fiction" } } }
+      sort: { fields: data___people___data___last_name, order: ASC }
+    ) {
+      edges {
+        node {
+          id
+          data {
+            collection
+            goodreads
+            year
+            title
+            featured
+            people {
+              data {
+                first_name
+                last_name
+              }
+            }
+            tags {
+              data {
+                tag
+              }
+            }
+            cover {
+              localFiles {
+                childImageSharp {
+                  fluid(maxWidth: 400) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Bookshelf
