3ad8b465b725e2773f8636f4b702e866e8d3f76f
iljapanic <hello@iljapanic.com>
2022-08-14 00:00:32 +0200

    update date formatting on post preview

--- components/PostPreview.js
+++ components/PostPreview.js
@@ -1,11 +1,12 @@
 import Link from 'next/link'
+import { displayDate } from '../lib/displayDate'
 
 export default function PostPreview({ title, date, summary, href }) {
   return (
     <article className="mt-12">
       {date && (
-        <div className="text-secondary dark:text-secondary-dark text-sm">
-          {date}
+        <div className="text-sm text-secondary dark:text-secondary-dark">
+          {displayDate(date)}
         </div>
       )}
       <h2 className="mt-1 mb-0 text-lg">
@@ -13,7 +14,7 @@ export default function PostPreview({ title, date, summary, href }) {
           <a className="no-underline">{title}</a>
         </Link>
       </h2>
-      {summary && <p className="text-secondary mb-0">{summary}</p>}
+      {summary && <p className="mb-0 text-secondary">{summary}</p>}
     </article>
   )
 }
